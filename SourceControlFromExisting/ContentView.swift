3292511fcf5773031a577dce1b20de5d922e7cbf
Mathew Gonzales <gonzalesmathewr@gmail.com>
2022-09-13 17:03:30 -0700

    Initial Commit

---
+++ SourceControlFromExisting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControlFromExisting
+//
+//  Created by Mathew Gonzales on 9/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControlFromExisting/SourceControlFromExistingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlFromExistingApp.swift
+//  SourceControlFromExisting
+//
+//  Created by Mathew Gonzales on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlFromExistingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
